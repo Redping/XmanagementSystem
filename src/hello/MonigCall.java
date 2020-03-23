1705bc77e36523e23fae17befda781bd2fc70253
fub <crack2817@gmail.com>
2020-03-23 11:42:37 +0900

    Add menu

---
+++ src/hello/MonigCall.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class MonigCall {
+	public static void main (String []args)
+	{	
+		System.out.println("MENU ---------");
+		
+		System.out.println("1.Add Url");
+		System.out.println("2.Delete Url");
+		System.out.println("3.Edit Url");
+		System.out.println("4.View Url");
+		System.out.println("5.Exit");
+		
+		System.out.println("MENU ---------\n");
+		
+		System.out.println("Select Number!!");
+	}
+}
