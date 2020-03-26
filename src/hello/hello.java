60cb67f88c3c0ef078730c88298061be358680b5
Redping <crack2817@gmail.com>
2020-03-26 21:14:14 +0900

    This is test

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -1,7 +1,20 @@
 package hello;
 
+import java.util.Scanner;
+
 public class hello {
 	public static void main(String[]args) {
+		Scanner input =new Scanner(System.in);
+		int num =5;
+			
+		System.out.println("1. Add url");
+		System.out.println("2. Delete Student");
+		System.out.println(". Edit Student");
+		System.out.println("3. Edit Student");
+		System.out.println("4. View Student");
+		System.out.println("5.Show a menu");
+		System.out.println("6.Exit");
+		
 		System.out.println("hello!");
 	}
 }
